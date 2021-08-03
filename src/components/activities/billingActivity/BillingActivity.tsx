cd733d55e4db9c8c0148ca85f720479cc905676c
zakariamoustaid <zakaria.moustaid@edu.unife.it>
2021-08-03 21:14:37 +0200

    aggiunte prime traduzioni e form pagina

--- src/components/activities/billingActivity/BillingActivity.tsx
+++ src/components/activities/billingActivity/BillingActivity.tsx
@@ -1,8 +1,11 @@
 import React, { FunctionComponent, useEffect, useRef } from "react";
 import AppHeader from "../../accessories/appHeader/AppHeader";
+import Footer from "../../accessories/footer/Footer";
+import Button from "@material-ui/core/Button";
 import { IState } from "../../../types";
 import { IStateProps, TProps } from "./types";
 import { useTranslation } from "react-i18next";
+import "./styles.scss";
 
 const BillingActivity: FunctionComponent<TProps> = ({
     userCredentials
@@ -14,7 +17,31 @@ const BillingActivity: FunctionComponent<TProps> = ({
         [t("nav.billing")]: "/bill",
     };
     return (
-        <h1>ciao!</h1>
+        <div className="billing">
+            <AppHeader
+                userCredentials={userCredentials}
+                breadcrumbMap={breadcrumbMap}
+            />
+            <div className="billing__background">
+                <div className="container">
+                    <div className="billing__title">{t("nav.billing")}</div>
+                    <form className="billing__panel">
+                        <h3>{t("nav.bill_chose")}</h3>
+                        <div className="bill_buttons__Container">
+
+                            <Button className="new_bill__button" variant="outlined" size="large">
+                                {t("nav.newbill")}
+                            </Button>
+                            <Button className="search_bill__button" variant="outlined" size="large">
+                                {t("nav.searchbill")}
+                            </Button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <Footer />
+        </div>
+
     )
 }
 
